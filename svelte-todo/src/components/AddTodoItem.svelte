d4640c08962e72cf84b99afcc4711e354f53bc9e
i1-kmv <i1-kmv@mail.ru>
2021-11-23 12:48:43 +0300

    lifecycle methods in svetle

--- svelte-todo/src/components/AddTodoItem.svelte
+++ svelte-todo/src/components/AddTodoItem.svelte
@@ -1,23 +1,48 @@
 <script>
+    import {onMount, tick} from 'svelte'
+    import {getTodos} from '../utils/getTodos'
+    import {format} from '../utils/format'
     export let title = 'Enter what do you want todo:'
     export let buttonTitle = 'Add todo'
 
     let items = []
+  
+    onMount(() => {
+      const get = async () => {
+          items = await getTodos()
+      }
+      get()
+    })
 
     function handleAddClick() {
         items = [...items, 'item']
     }
 
-    $: items
+    $: console.log(items)
+
+    let text = ''
+    async function handleChangeText(event) {
+        const {selectionStart, selectionEnd, value} = this
+        text = format(event.target.value)
+
+        await tick()
+        this.selectionStart = selectionStart
+        this.selectionEnd = selectionEnd
+    }
+
 </script>
-
-
 
 <div class='main-container'>
     <label for="todo=text">{title}</label>
-    <input class='todo-input' id="todo-text">
+    <input 
+        class='todo-input'
+        id="todo-text" 
+        on:input={handleChangeText}
+        value={text}
+      />
     <button on:click={handleAddClick}>{buttonTitle}</button>
 </div>
+{JSON.stringify(items)}
 
 
 <style>
