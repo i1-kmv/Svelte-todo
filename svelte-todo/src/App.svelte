f9c6e2850bc0d6840f329833bdbe45ecf37740ce
i1-kmv <i1-kmv@mail.ru>
2021-11-23 11:55:38 +0300

    work with components

--- svelte-todo/src/App.svelte
+++ svelte-todo/src/App.svelte
@@ -1,11 +1,13 @@
 <script>
+	import AddTodoItem from "./components/AddTodoItem.svelte";
 	export let name;
 </script>
 
-
+<main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
+</main>
+<AddTodoItem/>
 
 <style>
 	main {
---
+++ svelte-todo/src/components/AddTodoItem.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let title = 'Enter what do you want todo:'
+    export let buttonTitle = 'Add todo'
+
+    let items = []
+
+    function handleAddClick() {
+        items = [...items, 'item']
+    }
+
+    $: items
+</script>
+
+
+
+<div class='main-container'>
+    <label for="todo=text">{title}</label>
+    <input class='todo-input' id="todo-text">
+    <button on:click={handleAddClick}>{buttonTitle}</button>
+</div>
+
+
+<style>
+    .main-container {
+        background-color: lightseagreen;
+        border-radius: 5px;
+        padding: 10px;
+    }
+    .todo-input {
+        width: 100%;
+    }
+</style>
